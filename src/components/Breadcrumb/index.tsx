b2ae77f28d60d66e9ad9ff5a4b9b46e7efa71065
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2026-06-19 15:21:35 +0330

    refactor: breadcrumb

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,26 +1,33 @@
+import { ChevronLeft } from 'lucide-react';
 import Link from 'next/link';
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { BreadcrumbProps } from './breadcrumb.type';
 
 const Breadcrumb = ({ breadcrumb }: { breadcrumb: BreadcrumbProps[] }) => {
     return (
-        <div className='text-primary font-bold text-xl mb-12 flex items-center justify-between'>
-            <div className='flex gap-x-1'>
-                <Link href='/'>Home</Link>
-                <p>/</p>
+        <div className='text-secondary-400 mb-12 text-sm flex items-center justify-between'>
+            <div className='flex gap-x-1 items-center'>
+                <div className='group flex gap-x-1 items-center'>
+                    <Link href='/'>خانه</Link>
+                    <ChevronLeft className='size-5 transition-transform duration-300 delay-75 group-hover:rotate-180' />
+                </div>
                 {breadcrumb.map((item, index) => (
                     <Fragment key={index}>
-                        <Link
-                            href={item.link}
-                            className={
-                                index == breadcrumb.length - 1
-                                    ? 'text-secondary'
-                                    : ''
-                            }
-                        >
-                            {item.title}
-                        </Link>
-                        {index !== breadcrumb.length - 1 && <p>/</p>}
+                        <div className='flex items-center gap-x-1 group'>
+                            <Link
+                                href={item.link}
+                                className={
+                                    index == breadcrumb.length - 1
+                                        ? 'text-primary'
+                                        : 'text-secondary-400 group-hover:text-primary transition-colors'
+                                }
+                            >
+                                {item.title}
+                            </Link>
+                            {index !== breadcrumb.length - 1 && (
+                                <ChevronLeft className='size-5 transition-transform duration-300  delay-75 group-hover:rotate-180' />
+                            )}
+                        </div>
                     </Fragment>
                 ))}
             </div>
